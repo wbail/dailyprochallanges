97240398252a18db3c6e8a592a75503bba7208ad
gdabail <guilherme.d.bail@exxonmobil.com>
2020-03-25 16:14:31 -0300

    FindNonDuplicateNumber solved

---
+++ FindNonDuplicateNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace FindNonDuplicateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = {9, 5, 4, 7, 5, 9, 4, 7, 1};
+
+            Console.WriteLine(FindUniqueNumber(a));
+        }
+
+        static int FindUniqueNumber(int[] arr)
+        {
+            int count = 1, result = 0, n = arr.Count();
+
+            for(int i = 0; i < n; i++)
+            {
+                int ind = i % n;
+                for(int j = 0; j < n; j++)
+                {
+                    if(arr[ind] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if(count < 2) {
+                    result = arr[ind];
+                }
+
+                count = 0;
+            }
+
+            return result;
+        }
+    }
+}
